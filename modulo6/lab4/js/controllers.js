887d0125e1975f2df3fad6efa18d20cd9f0f8ac6
MAURICIO SAUCEDO CAMARGO <mauriciosauced@hotmail.com>
2016-07-30 12:49:41 -0500

    primer commit

---
+++ modulo6/lab4/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('mAUROCtrl', function($scope) {
+
+})
+   
+.controller('pageCtrl', function($scope) {
+
+})
+   
+.controller('page2Ctrl', function($scope) {
+
+})
+   
+.controller('page3Ctrl', function($scope) {
+
+})
+ 
---
+++ modulo6/lab4/js/routes.js
@@ -0,0 +1,41 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('mAURO', {
+    url: '/page1',
+    templateUrl: 'templates/mAURO.html',
+    controller: 'mAUROCtrl'
+  })
+
+  .state('page', {
+    url: '/page2',
+    templateUrl: 'templates/page.html',
+    controller: 'pageCtrl'
+  })
+
+  .state('page2', {
+    url: '/page4',
+    templateUrl: 'templates/page2.html',
+    controller: 'page2Ctrl'
+  })
+
+  .state('page3', {
+    url: '/page5',
+    templateUrl: 'templates/page3.html',
+    controller: 'page3Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page1')
+
+  
+
+});
